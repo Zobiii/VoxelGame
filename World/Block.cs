253e3093a7ea956658ba99d715dc3fb2b52ee6b1
Tobe <61782081+Zobiii@users.noreply.github.com>
2025-08-16 15:18:33 +0200

    added block representation

---
+++ World/Block.cs
@@ -0,0 +1,11 @@
+namespace VoxelGame.World;
+
+//// <summary>
+/// Minimal block representation. For now all solid white.
+/// </summary>
+
+public enum Block : byte
+{
+    Air = 0,
+    Solid = 1,
+}
